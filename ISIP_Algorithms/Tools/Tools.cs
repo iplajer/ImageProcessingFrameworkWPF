c78df849d0e9886ce8f63d37fad3484dd4fb58d9
iplajer <72392078+iplajer@users.noreply.github.com>
2020-10-08 11:19:14 +0300

    Add files via upload

---
+++ ISIP_Algorithms/Tools/Tools.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Emgu.CV;
+using Emgu.CV.Structure;
+namespace ISIP_Algorithms.Tools
+{
+    public class Tools
+    {
+        public static Image<Gray, byte> Invert(Image<Gray, byte> InputImage)
+        {
+
+            Image<Gray, byte> Result = new Image<Gray, byte>(InputImage.Size);
+            for (int y = 0; y < InputImage.Height; y++)
+            {
+                for (int x = 0; x < InputImage.Width; x++)
+                {
+                    Result.Data[y, x, 0] = (byte)(255 - InputImage.Data[y, x, 0]);
+                }
+            }
+            return Result;
+        }
+    }
+}
